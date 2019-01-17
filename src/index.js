627a3ad1b3ebb3402ee3f2e9e9d9c3ee3b350334
Dimitri Kurashvili <dimakura@gmail.com>
2019-01-17 21:56:50 +0400

    Moving to new folder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export const Config = require('./Config').default
-export const createSchema = require('./Schema').default
-export const createField = require('./Schema/Field').default
-export const createFieldType = require('./Schema/FieldType').default
+export const config = require('./config').default
+export const createSchema = require('./schema').default
+export const createField = require('./schema/Field').default
+export const createFieldType = require('./schema/FieldType').default

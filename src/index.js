261973f1c7ee3fad49f4290f4f9098d89b0d2aca
Dimitri Kurashvili <dimakura@gmail.com>
2019-01-17 21:57:43 +0400

    Convert to lower case naming (#4)
    * Moving to new folder
    
    * Moving to new folder

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export const Config = require('./Config').default
-export const createSchema = require('./Schema').default
-export const createField = require('./Schema/Field').default
-export const createFieldType = require('./Schema/FieldType').default

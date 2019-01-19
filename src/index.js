40606cb7f3528561a387371eb660d014d12e5cf6
Dimitri Kurashvili <dimakura@gmail.com>
2019-01-19 14:10:20 +0400

    Table data source

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export const config = require('./config').default
 export const createSchema = require('./schema').default
+
+// XXX: do we need to export this? Probably no!
 export const createField = require('./schema/Field').default
 export const createFieldType = require('./schema/FieldType').default

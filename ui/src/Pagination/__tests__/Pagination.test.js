6b352fc668af03ef1d12e92cab0f0ef9d58ef04c
Dimitri Kurashvili <dimakura@gmail.com>
2019-09-03 19:15:42 +0400

    Pagination specs

---
+++ ui/src/Pagination/__tests__/Pagination.test.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { merge } from 'lodash'
+import { Button } from '@blueprintjs/core'
+import Pagination from '..'
+
+describe('<Pagination />', () => {
+  const dataSource = {
+    addStatusListener: () => () => {}
+  }
+
+  test('at the first page', () => {
+    const pageExtractor = getPageExtractor()
+    const pageMover = getPageMover()
+
+    const comp = mount(<Pagination
+      dataSource={dataSource}
+      pageExtractor={pageExtractor}
+      pageMover={pageMover}
+    />)
+
+    const buttons = comp.find(Button)
+    expect(buttons).toHaveLength(4)
+    expect(buttons.at(0).prop('disabled')).toBe(true)
+    expect(buttons.at(1).prop('disabled')).toBe(true)
+    expect(buttons.at(2).prop('disabled')).toBe(false)
+    expect(buttons.at(3).prop('disabled')).toBe(false)
+
+    expect(comp.find('div.rf-pages').text()).toBe('Page 1 of 10')
+
+    buttons.at(2).simulate('click')
+    buttons.at(3).simulate('click')
+    expect(pageMover).toHaveBeenCalledWith(dataSource, 10)
+  })
+
+  test('at a middle page', () => {
+    const pageExtractor = getPageExtractor({ page: 5 })
+    const pageMover = getPageMover()
+
+    const comp = mount(<Pagination
+      dataSource={dataSource}
+      pageExtractor={pageExtractor}
+      pageMover={pageMover}
+    />)
+
+    const buttons = comp.find(Button)
+    expect(buttons).toHaveLength(4)
+    expect(buttons.at(0).prop('disabled')).toBe(false)
+    expect(buttons.at(1).prop('disabled')).toBe(false)
+    expect(buttons.at(2).prop('disabled')).toBe(false)
+    expect(buttons.at(3).prop('disabled')).toBe(false)
+
+    expect(comp.find('div.rf-pages').text()).toBe('Page 5 of 10')
+
+    buttons.at(0).simulate('click')
+    buttons.at(1).simulate('click')
+    buttons.at(2).simulate('click')
+    buttons.at(3).simulate('click')
+    expect(pageMover).toHaveBeenCalledWith(dataSource, 4)
+    expect(pageMover).toHaveBeenCalledWith(dataSource, 1)
+    expect(pageMover).toHaveBeenCalledWith(dataSource, 6)
+    expect(pageMover).toHaveBeenCalledWith(dataSource, 10)
+  })
+
+  test('at the last page', () => {
+    const pageExtractor = getPageExtractor({ page: 10 })
+    const pageMover = getPageMover()
+
+    const comp = mount(<Pagination
+      dataSource={dataSource}
+      pageExtractor={pageExtractor}
+      pageMover={pageMover}
+    />)
+
+    const buttons = comp.find(Button)
+    expect(buttons).toHaveLength(4)
+    expect(buttons.at(0).prop('disabled')).toBe(false)
+    expect(buttons.at(1).prop('disabled')).toBe(false)
+    expect(buttons.at(2).prop('disabled')).toBe(true)
+    expect(buttons.at(3).prop('disabled')).toBe(true)
+
+    expect(comp.find('div.rf-pages').text()).toBe('Page 10 of 10')
+
+    buttons.at(0).simulate('click')
+    buttons.at(1).simulate('click')
+    expect(pageMover).toHaveBeenCalledWith(dataSource, 9)
+    expect(pageMover).toHaveBeenCalledWith(dataSource, 1)
+  })
+})
+
+const defaultPages = {
+  limit: 10,
+  total: 100,
+  page: 1,
+  pages: 10
+}
+
+function getPageExtractor(pages) {
+  return jest.fn(() => merge({}, defaultPages, pages))
+}
+
+function getPageMover() {
+  return jest.fn()
+}

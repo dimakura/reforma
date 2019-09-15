3fdc41f21ea3771a22cf0ab87e7f537695f37d68
Dimitri Kurashvili <dimakura@gmail.com>
2019-09-15 23:20:30 +0400

    Styling cells

--- packages/ui/src/render/__tests__/renderEmpty.test.js
+++ packages/ui/src/render/__tests__/renderEmpty.test.js
@@ -5,6 +5,6 @@ test('renderEmpty', () => {
   const comp = shallow(renderEmpty())
 
   expect(comp.name()).toBe('span')
-  expect(comp.prop('className')).toBe('bp3-text-muted')
+  expect(comp.prop('className')).toBe('rf-empty bp3-text-muted')
   expect(comp.text()).toBe('(empty)')
 })
--- packages/ui/src/render/renderEmpty.js
+++ packages/ui/src/render/renderEmpty.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function renderEmpty() {
   return (
-    <span className="bp3-text-muted">(empty)</span>
+    <span className="rf-empty bp3-text-muted">(empty)</span>
   )
 }
